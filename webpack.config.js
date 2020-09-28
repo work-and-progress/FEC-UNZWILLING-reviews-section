3c2f3214343241eaa7d337489e5d471ae5b58a85
Karin Aizawa <kaizawa77@gmail.com>
2020-09-28 14:03:41 -0700

    learnign about babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,6 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
-          },
-
         },
       },
     ],
