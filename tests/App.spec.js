86c6f661bd32ae95e935781cd3e89ce10ac06af7
Karin Aizawa <kaizawa77@gmail.com>
2020-09-30 15:15:07 -0700

    wrote some tests in App.spec but two are failing

--- tests/App.spec.js
+++ tests/App.spec.js
@@ -1,17 +1,47 @@
 import React from 'react';
+import { mount, shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
+
+/*---------------------------------------*/
 import App from '../client/src/components/App';
 
-describe('My Test Suite', () => {
-  test('My Test Case', () => {
-    expect(true).toEqual(true);
-  });
-});
+import MostHelpful from '../client/src/components/ReviewSummary/MostHelpful';
+import Overview from '../client/src/components/ReviewSummary/Overview';
+
+import NextPage from '../client/src/components/ReviewPagination/NextPage';
+import Sort from '../client/src/components/ReviewPagination/Sort';
+
+import Review from '../client/src/components/ReviewContent/Review';
 
 describe('App', () => {
   test('snapshot renders', () => {
     const component = renderer.create(<App />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  test('renders the Overview Component', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find(Overview).length).toEqual(1);
+  });
+
+  test('renders the MostHelpful Component', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find(MostHelpful).length).toEqual(1);
+  });
+
+  test('renders the NextPage Component', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find(NextPage).length).toEqual(1);
+  });
+
+  test('renders the Sort Component', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find(Sort).length).toEqual(1);
+  });
+
+  test('renders the Review Component', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find(Review).length).toEqual(1);
   });
 });
