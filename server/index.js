9b88352302b494402eb2b2753ad69ec9d6e466ec
Karin Aizawa <kaizawa77@gmail.com>
2020-10-05 16:03:37 -0700

    proxy stuff

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const database = require('../database/index.js');
 
 const app = express();
-const port = 3000;
+const port = 7777;
 
 const path = require("path");
 app.use(express.static(path.join(__dirname, "../client/dist")));
